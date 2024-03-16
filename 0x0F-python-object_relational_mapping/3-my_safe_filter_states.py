3f128f5c7d769cd8578801b780a9cf83a9b30823
aligomaa56 <3looosh56565656@gmail.com>
2024-03-16 11:10:50 +0200

    fix2

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""all values in the states in hbtn_0e_0_usa where name = argument"""
+"""all values in the states"""
 import MySQLdb
 from sys import argv
 
@@ -7,8 +7,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3], charset="utf8")
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE %s ORDER\
-        BY states.id ASC", (argv[4]))
+    cur.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
+                (argv[4],))
     rows = cur.fetchall()
     for row in rows:
         print(row)
